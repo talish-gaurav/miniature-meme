90ea7e23a77b6da3a786b9efd467611029d1bfef
talish-gaurav <97028189+talish-gaurav@users.noreply.github.com>
2024-10-30 19:44:23 +0000

    Add files via upload

---
+++ introduction.py
@@ -0,0 +1,11 @@
+print("Write the temperature")
+
+temper1=int(input("Enter degrees celcius in here"))
+
+if temper1 >=25:
+
+     print("It is advised you do not wear jumpers and stuffy clothing ")
+
+else:
+     
+      print("Wear a jumper ,if you go outside wear a jacket as well")
